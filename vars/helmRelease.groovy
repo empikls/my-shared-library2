b21cc1d91dc644f0e2bffebab1e3d5f0f9469685
empikls <52194104+empikls@users.noreply.github.com>
2020-03-12 17:35:27 +0200

    Update helmRelease.groovy

--- vars/helmRelease.groovy
+++ vars/helmRelease.groovy
@@ -1,6 +1,6 @@
 def call(String namespace, String releaseName, String valuesFile, String imageTag) {
     sh """
-            helm upgrade \
+            helm upgrade --dry-run --debug \
             --install $releaseName \
             --namespace $namespace \
             --force \
